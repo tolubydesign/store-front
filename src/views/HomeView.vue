576b4eba9c33735e3a635686fe91ac4e24fdee9a
Tolu Adesina <tolu.adesina79@gmail.com>
2023-09-30 16:55:40 +0200

    ADD first init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue';
+import Main from "@/components/pages/main/Main.vue";
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <Main /> 
+
+  </main>
+</template>
