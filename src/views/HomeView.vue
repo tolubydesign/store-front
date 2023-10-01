59de3694cbca901ac7c4ee19cbe753ba5315307f
Tolu Adesina <tolu.adesina79@gmail.com>
2023-10-01 17:39:37 +0200

    REFACTOR home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue';
-import Main from "@/components/pages/main/Main.vue";
+import Main from "@/components/pages/Main.vue";
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
     <Main /> 
-
   </main>
 </template>
